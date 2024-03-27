3e82fd17e637ed32be7c5f4c19df9a51b1347ee1
KhalidS3 <khalid.safi1@hotmail.com>
2024-03-27 11:51:37 +0100

    Successfully Implement Level 1.

---
+++ MiniProjects/Week13/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+class Products
+{
+    public Products()
+    {
+    }
+
+    public Products(string category, string productName, double price)
+    {
+        Category = category;
+        ProductName = productName;
+        Price = price;
+    }
+
+    public string Category { get; set; }
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+
+    public override string ToString()
+    {
+        CultureInfo swedish = new CultureInfo("sv-SE");
+        return $"{Category.PadRight(15)}{ProductName.PadRight(15)}{Price.ToString("C", swedish).PadRight(15)}";
+    }
+
+}
