3a97c26b80cddc25ed0aa0a521c68dc894ac66f1
KhalidS3 <khalid.safi1@hotmail.com>
2024-04-03 11:00:48 +0200

    Finished with Asset Tracking Level1

---
+++ MiniProjects/Week14/Asset.cs
@@ -0,0 +1,21 @@
+﻿class Asset
+{
+    public Asset(string modelName, DateTime purchaseDate, double price)
+    {
+        ModelName = modelName;
+        PurchaseDate = purchaseDate;
+        Price = price;
+    }
+
+    public string ModelName { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public double Price { get; set; }
+
+    // Method to check if the asset is at its end of life (3 years)
+    public bool IsEndOfLife()
+    {
+        return DateTime.Now.Year - PurchaseDate.Year >= 3;
+    }
+
+
+}
---
+++ MiniProjects/Week14/Laptop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjects.Week14
+{
+    internal class Laptop : Asset
+    {
+        public Laptop(string modelName, DateTime purchaseDate, double price)
+            : base(modelName, purchaseDate, price)
+        {
+        }
+  
+    }
+}
