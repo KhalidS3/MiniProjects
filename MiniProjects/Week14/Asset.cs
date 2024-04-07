3e242cd390d766f7ad7a781bdd4da180f02914b4
KhalidS3 <khalid.safi1@hotmail.com>
2024-04-07 21:19:17 +0200

    Finished with Week14 mini project. Fixed merge conflict in Asset.cs

--- MiniProjects/Week14/Asset.cs
+++ MiniProjects/Week14/Asset.cs
@@ -1,15 +1,41 @@
 ﻿namespace MiniProjects.Week14
 {
-    // I have the constructor here because both Laptop and MobilePhone classes will inherit from this class
+   // I have the constructor here because both Laptop and MobilePhone classes will inherit from this class
     // and both of them share same properties. By doing this, I can avoid dubplicating the properties in both classes.
     // This method is called DRY (Don't Repeat Yourself).
     // If there is a change in the properties of the specific class, I can change it directly in the specific class. 
-    public Asset(string modelName, DateTime purchaseDate, double price) 
+    // public Asset(string modelName, DateTime purchaseDate, double price) 
+    internal class Asset
     {
-        ModelName = modelName;
-        PurchaseDate = purchaseDate;
-        Price = price;
-    }
+        //public Asset(string officeLocation, string currency) : base(officeLocation, currency)
+        //{
+        //}
+
+        // I have the constructor here because both Laptop and MobilePhone classes will inherit from this class
+        // and both of them share same properties. By doing this, I can avoid dubplicating the properties in both classes.
+        // This method is called DRY (Don't Repeat Yourself).
+        // If there is a change in the properties of the specific class, I can change it directly in the specific class. 
+        //public Asset(string modelName, DateTime purchaseDate, double price) 
+        //{
+        //    ModelName = modelName;
+        //    PurchaseDate = purchaseDate;
+        //    Price = price;
+        //}
+        public string? Brand { get; set; }
+        public string? ModelName { get; set; }
+        public DateTimeOffset PurchaseDate { get; set; }
+        public double Price { get; set; }
+        public Office? Office { get; set; } // Office is inherited from the Office class, because it not needed.
+
+        // Method to check if the asset is at its end of life (3 years)
+        public int MonthsToEndOfLife()
+        {
+            var monthsToEnd = (PurchaseDate.AddYears(3).AddMonths(-3) - DateTime.Now).Days / 30; // Explian the logic
+            return (int)monthsToEnd;
+        }
+        public override string ToString()
+        {
+            double localPriceToday = ConvertToLocalCurrency(Price);
 
             return $"{GetType().Name.PadRight(15)}{Brand,-15}{ModelName?.PadRight(15)}" +
                    $"{Office?.OfficeLocation.PadRight(15)}{PurchaseDate.ToString("yyyy-MM-dd").PadRight(20)}" +
